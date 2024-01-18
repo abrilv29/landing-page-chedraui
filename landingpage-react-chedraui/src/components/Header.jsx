3d411ced431d7cffc75839dacd216e20db50aa4e
Abril <abrilv29@gmail.com>
2024-01-17 22:49:10 -0600

    slider del home

--- landingpage-react-chedraui/src/components/Header.jsx
+++ landingpage-react-chedraui/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./NavBar";
+import BannerHeader from "./BannerHeader";
 
 function Header() {
       return (
         <div>
           <NavBar/>
+          <BannerHeader />
           Header
       
 
